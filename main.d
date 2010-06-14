0281ec277336194c7ddfa3d985d2b6e5f0d4a1b7
Justin Spahr-Summers <Justin Spahr-Summers@localhost>
2010-06-13 18:24:00 -0700

    Remove existing lexing stuff, move runtime to a new 'lib' folder

--- main.d
+++ main.d
@@ -1,14 +1,4 @@
 import std.stdio;
-import objd;
-
-Class MyClass;
-
-static this () {
-    MyClass = new Class("MyClass", NSObject);
-}
 
 void main () {
-    id obj = MyClass.msgSend!(id)("new");
-    writefln("%s", obj.msgSend!(string)("description"));
-    writefln("%s", MyClass.msgSend!(string)("description"));
 }
